68f0c0b78ec1d40a863844bebf0ec4296454c80e
Matheus <matheusgomesrodrigues96@gmail.com>
2025-05-15 21:54:07 -0300

    feat: Conteúdo Parte01

---
+++ calculadora_soma.py
@@ -0,0 +1,6 @@
+# Calcular dois números
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o primeiro número: "))
+soma = numero1 + numero2
+print(f"A soma do número1 '{numero1}' e do numero2 '{numero2}' é soma '{soma}'")
