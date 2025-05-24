5d386aff89da54a0760273e16fcccd1367680c11
Matheus <matheusgomesrodrigues96@gmail.com>
2025-05-23 21:26:52 -0300

    prática 03 modificado

--- praticar-03/conversor_de_temperatura.py
+++ praticar-03/conversor_de_temperatura.py
@@ -3,28 +3,28 @@ Crie um programa que converta temperaturas entre Celsius, Fahrenheit e Kelvin.
 O usuário deve informar a temperatura, a unidade de origem e a unidade para qual deseja converter.
 """
 
- 
-print("Escolha a conversão:")
-print("1 - Celsius para Fahrenheit")
-print("2 - Celsius para Kelvin")
-print("3 - Fahrenheit para Celsius")
 
-opcao = input("Digite a opção (1, 2 ou 3): ")
+temp = float(input("Digite a temperatura: "))
+origem = input("Digite a unidade de origem (C, F ou K): ").upper()
+destino = input("Digite a unidade de destino (C, F ou K): ").upper()
 
-if opcao == "1":
-    c = float(input("Digite a temperatura em Celsius: "))
-    f = c * 9/5 + 32
-    print(f"Fahrenheit: {f:.2f}")
+if origem == destino:
+    resultado = temp
+elif origem == "C":
+    if destino == "F":
+        resultado = (temp * 9/5) + 32
+    else:
+        resultado = temp + 273.15
 
-elif opcao == "2":
-    c = float(input("Digite a temperatura em Celsius: "))
-    k = c + 273.15
-    print(f"Kelvin: {k:.2f}")
-
-elif opcao == "3":
-    f = float(input("Digite a temperatura em Fahrenheit: "))
-    c = (f - 32) * 5/9
-    print(f"Celsius: {c:.2f}")
-
+elif origem == "F":
+    if destino == "C":
+        resultado = (temp - 32) / 9/5
+    else:
+        resultado = (temp - 32) / 9/5 + 273.15
 else:
-    print("Opção inválida")
+    if destino == "C":
+        resultado = temp - 273.15
+    else:
+        resultado = (temp - 273.15) * 9/5 + 32
+
+print(f"A temperarura {temp} {origem} é igual a {resultado:.2f} {destino}")
